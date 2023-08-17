faac492c4b69b1132f1fc8619d3ca81bb8fa4ee5
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-08-17 20:15:22 -0300

    modificando projeto no html

--- JAVASCRIPT/ARMAZENANDO-DADOS-NO-NAVEGADOR/mochila-de-viagem/js/script.js
+++ JAVASCRIPT/ARMAZENANDO-DADOS-NO-NAVEGADOR/mochila-de-viagem/js/script.js
@@ -1,47 +1,75 @@
 const frm = document.querySelector("#novoItem")
 const lista = document.querySelector(".lista")
+//localStorage
 const itens = JSON.parse(localStorage.getItem("itens")) || []
 
 itens.forEach((e) => {
-    console.log(e)
+    criaElemento(e)
 })
 
-frm.addEventListener("submit", (e) => {
-    e.preventDefault()
+frm.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+    //variaveis
+    const nome = evento.target.elements['nome']
+    const qtd = evento.target.elements['quantidade']
 
-    const nome = e.target.elements['nome'].value
-    const qtd = e.target.elements['quantidade'].value
+    //LocalStorage
+    const existe = itens.find(elemento => elemento.nome === nome.value) //vendo se tem o nome de itens no nome do form
 
-    criaElemento(nome, qtd)
+    //criando objeto com as informações de nome e quantidade
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": qtd.value
+    }
+
+
+    if (existe) {
+        itemAtual.id = existe.id
+        atualizaElemento(itemAtual)
+        itens[existe.id] = itemAtual
+    } else {
+        itemAtual.id = itens.length
+        criaElemento(itemAtual)
+        itens.push(itemAtual)
+    }
+
+    //adicionando o array no localSotorage
+    localStorage.setItem("itens", JSON.stringify(itens))
+
     nome.value = ""
     qtd.value = ""
 
 })
 
 
-function criaElemento(nome, qtd) {
+function criaElemento(item) {
     const novoItem = document.createElement("li")
     novoItem.classList.add("item")
     const numeroItem = document.createElement("strong")
-    numeroItem.innerHTML = qtd
-
+    numeroItem.dataset.id = item.id //dataset é para criar um data no html e o .id é o que vem sem seguida, entao .dataset.id = data-id
+    numeroItem.innerHTML = item.quantidade
 
     novoItem.appendChild(numeroItem) //appendChild coloca um elemento como filho de outro 
-    novoItem.innerHTML += nome
+    novoItem.innerHTML += item.nome
+    novoItem.appendChild(botaoDeleta(item.id))
 
     lista.appendChild(novoItem)
-    localStorage.setItem("nome", nome)
-    localStorage.setItem("quantidade", qtd)
+}
+function botaoDeleta(id){
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerHTML = "X"
 
-    //criando objeto com as informações de nome e quantidade
-    const itemAtual = {
-        "nome": nome,
-        "qunatidade": qtd
-    }
+    elementoBotao.addEventListener("click", function(){
+        deleteElemento(this.parentNode, id)
+    })
+    return elementoBotao
+}
 
-    //adicionando o objeto no array 
-    itens.push(itemAtual)
+function deleteElemento(tag, id){
+    tag.remove()
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+}
 
-    //adicionando o array no localSotorage
-    localStorage.setItem("itens", JSON.stringify(itens))
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
 }
