af77030368dafb993df3bb46ec54dcf275dcf54f
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-08-21 20:48:05 -0300

    atividades feitas

--- JAVASCRIPT/ARMAZENANDO-DADOS-NO-NAVEGADOR/mochila-de-viagem/js/script.js
+++ JAVASCRIPT/ARMAZENANDO-DADOS-NO-NAVEGADOR/mochila-de-viagem/js/script.js
@@ -26,9 +26,9 @@ frm.addEventListener("submit", (evento) => {
     if (existe) {
         itemAtual.id = existe.id
         atualizaElemento(itemAtual)
-        itens[existe.id] = itemAtual
+        itens[existe.findIndex(elemento => elemento.id === existe.id)] = itemAtual
     } else {
-        itemAtual.id = itens.length
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0
         criaElemento(itemAtual)
         itens.push(itemAtual)
     }
