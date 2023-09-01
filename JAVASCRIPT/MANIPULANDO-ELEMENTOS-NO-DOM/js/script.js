cbc622c8cb535b781ddf225c3dd6cd31dba7acdf
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-09-01 19:11:09 -0300

    introdução ao bootstrap

--- JAVASCRIPT/MANIPULANDO-ELEMENTOS-NO-DOM/js/script.js
+++ JAVASCRIPT/MANIPULANDO-ELEMENTOS-NO-DOM/js/script.js
@@ -6,34 +6,46 @@ const banner = document.querySelector('.app__image')
 const texto = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
 const musicaFocoInput = document.querySelector('#alternar-musica')
-
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
-musica.loop = true
+const timer = document.querySelector("#timer")
 
-musicaFocoInput.addEventListener('change', ()=>{
-    if(musica.paused){
+musica.loop = true
+const btnIniciarOuPausar = document.querySelector("#start-pause span")
+const img = document.querySelector("#start-pause img")
+
+let tempoEmSegundos = 1500
+let intervalo = null
+
+const btnIniciar = document.querySelector("#start-pause")
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
         musica.play()
-    }else{
+    } else {
         musica.pause()
     }
 })
 btnFoco.addEventListener('click', () => {
+    tempoEmSegundos = 1500
     alterarContexto('foco')
     btnFoco.classList.add('active')
 })
 
 btnCurto.addEventListener('click', () => {
+    tempoEmSegundos = 300
     alterarContexto('descanso-curto')
     btnCurto.classList.add('active')
 })
 
 btnLongo.addEventListener('click', () => {
+    tempoEmSegundos = 900
     alterarContexto('descanso-longo')
     btnLongo.classList.add('active')
 })
 
 function alterarContexto(contexto) {
-    botoes.forEach( (btn) => btn.classList.remove('active'))
+    mostrarTempo()
+    botoes.forEach((btn) => btn.classList.remove('active'))
     html.setAttribute('data-contexto', contexto) //setAtribute('','') primeiro parametro é o atributo que quer ser modificado, o segundo é o conteúdo desse parametro
     banner.setAttribute('src', `imagens/${contexto}.png`)
 
@@ -57,3 +69,39 @@ function alterarContexto(contexto) {
             break
     }
 }
+
+
+const contagemRegressiva = () => {
+    /* iniciar() */
+    if (tempoEmSegundos <= 0) {
+        zerar()
+        alert('tempo finalizado ')
+        return
+    }
+    tempoEmSegundos -= 1
+    mostrarTempo()
+}
+
+
+btnIniciar.addEventListener('click', iniciarOuPausar)
+function iniciarOuPausar() {
+    if(intervalo){
+        zerar()
+        return
+    }
+    intervalo = setInterval(contagemRegressiva, 1000)
+    btnIniciarOuPausar.textContent = "Pausar"
+}
+
+function zerar() {
+    clearInterval(intervalo)
+    btnIniciarOuPausar.textContent = "Começar"
+    intervalo = null
+}
+
+function mostrarTempo(){
+    const tempo = new Date(tempoEmSegundos * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'})
+    timer.innerHTML = `${tempoFormatado}`
+}
+mostrarTempo()
