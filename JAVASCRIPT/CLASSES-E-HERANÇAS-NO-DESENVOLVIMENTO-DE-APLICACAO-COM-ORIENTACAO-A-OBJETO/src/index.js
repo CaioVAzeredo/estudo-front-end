810be19a40f74f8ea7024127d290904b7507536d
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-09-14 22:17:31 -0300

    adicionado cartas dos personagens

--- JAVASCRIPT/CLASSES-E-HERANÇAS-NO-DESENVOLVIMENTO-DE-APLICACAO-COM-ORIENTACAO-A-OBJETO/src/index.js
+++ JAVASCRIPT/CLASSES-E-HERANÇAS-NO-DESENVOLVIMENTO-DE-APLICACAO-COM-ORIENTACAO-A-OBJETO/src/index.js
@@ -1,21 +1,10 @@
 import { Personagem } from "./modules/personagem.js";
+import { PersonagemView } from "./components/personagem-view.js";
 
-const personagemJose = {
-    nome :'Jose' ,
-    vida: 7,
-    mana: 6,
-    level: 3,
-    tipo: 'Arqueiro',
-}
 
-const personagemAna = {
-    nome :'Ana' ,
-    vida: 7,
-    mana: 6,
-}
-const personagemPedrinho = new Personagem()
-personagemPedrinho.nome =' Pedrinho'
-personagemPedrinho.mana = '12'
-personagemPedrinho.vida = 7
 
-console.log(personagemPedrinho)
+const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago')
+const personagemJose = new Personagem('Jose', 3, 'Arqueiro')
+
+const personagens = [personagemPedrinho, personagemJose]
+new PersonagemView(personagens).render()
--- JAVASCRIPT/CLASSES-E-HERANÇAS-NO-DESENVOLVIMENTO-DE-APLICACAO-COM-ORIENTACAO-A-OBJETO/src/modules/personagem.js
+++ JAVASCRIPT/CLASSES-E-HERANÇAS-NO-DESENVOLVIMENTO-DE-APLICACAO-COM-ORIENTACAO-A-OBJETO/src/modules/personagem.js
@@ -1,8 +1,22 @@
-export class Personagem{
-    nome 
-    vida
-    mana
-    level
-    tipo
+export class Personagem {
+    nome // nao precisa ser declarado fora do constructor, é opcional
+    vida = 100
+    mana = 100
+    level // nao precisa ser declarado fora do constructor, é opcional
+    tipo // nao precisa ser declarado fora do constructor, é opcional
     descricao
+
+    constructor(nome, level, tipo) {
+        this.nome = nome
+        this.level = level
+        this.tipo = tipo
+        
+    }
+
+    obterInsigina() {
+        if (this.level >= 5) {
+            return `Implacavel ${this.tipo}`
+        }
+        return `${this.tipo} iniciante`
+    }
 }
