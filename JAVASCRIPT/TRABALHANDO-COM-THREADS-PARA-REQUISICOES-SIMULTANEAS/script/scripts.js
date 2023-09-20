5959ef619309fb4d23b24ab730ee4324ede40731
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-09-20 12:37:54 -0300

    .

--- JAVASCRIPT/TRABALHANDO-COM-THREADS-PARA-REQUISICOES-SIMULTANEAS/script/scripts.js
+++ JAVASCRIPT/TRABALHANDO-COM-THREADS-PARA-REQUISICOES-SIMULTANEAS/script/scripts.js
@@ -2,7 +2,7 @@
 //Conceitos sobre sigle thread e multi thread 
 //Conceito sobre web workers
 
-import imprimeCotacao from "./imprimeCotacao.js";
+import selecionaCotacao from "./imprimeCotacao.js";
 
                             /* DOLAR */
 const graficoDolar = document.querySelector('#graficoDolar')
@@ -18,13 +18,30 @@ const graficoParaDolar = new Chart(graficoDolar, {
     },
 });
 
+                        /* FUNÇÕES */
+/* gerar horario em tempo real  */
+function geraHorario() {
+    const data = new Date()
+    const horario = `${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}`
+    return horario
+}
+
+/* adicionar dados na tela */
+function adicionarDados(grafico, legenda, dados) {
+    grafico.data.labels.push(legenda)
+    grafico.data.datasets.forEach((dataset) => {
+        dataset.data.push(dados)
+    })
+    grafico.update()
+}
+
 let workerDolar = new Worker('./script/workers/workerDolar.js')
 workerDolar.postMessage('usd')
 
 workerDolar.addEventListener("message", e => {
     let tempo = geraHorario()
     let valor = e.data.ask
-    imprimeCotacao("dolar", valor)
+    selecionaCotacao("dolar", valor)
     adicionarDados(graficoParaDolar, tempo, valor)
 })
 
@@ -46,52 +63,9 @@ workerIene.postMessage('iene')
 workerIene.addEventListener("message", e =>{
     let tempo = geraHorario()
     let valor = e.data.ask
-    imprimeCotacao("dolar", valor)
+    selecionaCotacao("iene", valor)
     adicionarDados(graficoParaIene, tempo, valor)
 })
 
 
-                        /* FUNÇÕES */
-/* gerar horario em tempo real  */
-function geraHorario() {
-    const data = new Date()
-    const horario = `${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}`
-    return horario
-}
 
-/* adicionar dados na tela */
-function adicionarDados(grafico, legenda, dados) {
-    grafico.data.labels.push(legenda)
-    grafico.data.datasets.forEach((dataset) => {
-        dataset.data.push(dados)
-    })
-    grafico.update()
-}
-
-
-
-
-
-/* function geraHorario() {
-    const data = new Date()
-    const horario = `${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}`
-    return horario
-}
-
-
-function adicionarDados(grafico, legenda, dados) {
-    grafico.data.labels.push(legenda)
-    grafico.data.datasets.forEach((dataset) => {
-        dataset.data.push(dados)
-    })
-    grafico.update()
-}
-
-
-
-workerIene.addEventListener("message", e => {
-    let tempo = geraHorario()
-    let valor = e.data.ask
-    imprimeCotacao("dolar", valor)
-    adicionarDados(graficoParaDolar, tempo, valor)
-}) */
--- JAVASCRIPT/bots.py
+++ JAVASCRIPT/bots.py
@@ -1,7 +1,17 @@
 import webbrowser
+import pyautogui
 import time
+import webbrowser, os
+
 
 webbrowser.open("https://ava.catolica.edu.br/d2l/login")
+time.sleep(3)
+pyautogui.press('Tab')
+time.sleep(3)
+pyautogui.press('enter')
+time.sleep(5)   
 webbrowser.open("https://cursos.alura.com.br/dashboard")
 
+vscode ="D:\ARQUIVOS DE PROGRAMAS (X86)\Microsoft VS Code\code.exe"
+webbrowser.open(os.path.realpath(vscode))
 print("fim")
