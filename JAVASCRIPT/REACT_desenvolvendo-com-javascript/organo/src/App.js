6e90d8f535ac3b715d05b1b842775964a9a20a2d
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2024-03-19 22:19:37 -0300

    refazendo a atividade do zero

--- JAVASCRIPT/REACT_desenvolvendo-com-javascript/organo/src/App.js
+++ JAVASCRIPT/REACT_desenvolvendo-com-javascript/organo/src/App.js
@@ -1,63 +1,9 @@
-import { useState } from 'react';
-import Banner from './componentes/Banner/Banner.js';
-import Formulario from './componentes/Formulario/Formulario.js';
-import Time from './componentes/Time/index.js';
-import Rodape from './componentes/Rodape/index.js';
+import Banner from './componentes/Banner/Banner';
 
 function App() {
-
-  const times = [
-    {
-      nome: 'Front-End',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF',
-    },
-    {
-      nome: 'Data Sciense',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2',
-    },
-    {
-      nome: 'Devops',
-      corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8',
-    },
-    {
-      nome: 'UX e Design',
-      corPrimaria: '#D86EBF',
-      corSecundaria: '#FAE5F5',
-    },
-    {
-      nome: 'Mobile',
-      corPrimaria: '#FEBA05',
-      corSecundaria: '#FFF5D9',
-    },
-    {
-      nome: 'Inovação e Gestão',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF',
-    }
-  ]
-
-  const [colaboradores, setColaboradores] = useState([])
-
-  const aoNovoColaboradorAdicionado = (colaborador) => {
-    setColaboradores([...colaboradores, colaborador]) /* Pegando o que ja tinha na array '...colaboradores' e adicionando um novo com 'colaborador'*/
-  }
-
   return (
     <div className="App">
       <Banner />
-      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
-
-      {times.map(time => <Time
-        key={time.nome}
-        nome={time.nome}
-        corPrimaria={time.corPrimaria}
-        corSecundaria={time.corSecundaria}
-        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
-      />)}
-      <Rodape />
     </div>
   );
 }
