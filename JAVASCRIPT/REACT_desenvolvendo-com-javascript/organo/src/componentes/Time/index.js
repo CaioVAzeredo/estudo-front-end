7f5db5ed4c3b2bbbe33b104acaf041ed03ba5448
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2024-04-10 00:01:07 -0300

    adicionando time

--- JAVASCRIPT/REACT_desenvolvendo-com-javascript/organo/src/componentes/Time/index.js
+++ JAVASCRIPT/REACT_desenvolvendo-com-javascript/organo/src/componentes/Time/index.js
@@ -1,1 +1,13 @@
 import './time.css'
+
+const Time = (props) => {
+    const css = { backgroundColor: props.corSecundaria }
+
+    return (
+        <section className='time' style={css} /* style={{backgroundColor: props.corSecundaria}} */>
+            <h3 style={ {borderColor: props.corPrimaria}}>{props.nome}</h3>
+        </section>
+    )
+}
+
+export default Time
