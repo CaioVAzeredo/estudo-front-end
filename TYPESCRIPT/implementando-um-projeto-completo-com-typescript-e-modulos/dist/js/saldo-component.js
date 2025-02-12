af8cf20f486a7c052bbac1c3f2ff1adf0f51a0cb
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-02-12 11:55:48 -0300

    atividade em andamento

---
+++ TYPESCRIPT/implementando-um-projeto-completo-com-typescript-e-modulos/dist/js/saldo-component.js
@@ -0,0 +1,10 @@
+var saldo = 3000;
+var elementoSaldo = document.querySelector(".saldo-valor .valor");
+var elementoDataAcesso = document.querySelector(".block-saldo time");
+if (elementoSaldo != null) {
+    elementoSaldo.textContent = formatarMoeda(saldo);
+}
+if (elementoDataAcesso != null) {
+    const dataAcesso = new Date();
+    elementoDataAcesso.textContent = formatarData(dataAcesso);
+}
