245d3f374ca57f7f05312de3b7f868dbe97ee5b4
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-03-20 11:25:48 -0300

    adição de uma nova pasta

---
+++ ANGULAR/ANGULAR/evoluindo-com-formularios-e-roteamento1/src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContainerComponent } from "../../componentes/container/container.component";
+import { SeparadorComponent } from "../../componentes/separador/separador.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-formulario-contato',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ContainerComponent,
+    SeparadorComponent,
+    ReactiveFormsModule,
+    RouterLink
+  ],
+  templateUrl: './formulario-contato.component.html',
+  styleUrl: './formulario-contato.component.css'
+})
+export class FormularioContatoComponent {
+
+  contatoForm!: FormGroup;
+
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      aniversario: new FormControl('', Validators.required),
+      redes: new FormControl('', Validators.required),
+      obsercacoes: new FormControl('', Validators.required),
+    })
+  }
+
+  salvarContato() {
+    if (this.contatoForm.valid) {
+      console.log(this.contatoForm.value)
+    }
+  }
+  cancelar() {
+    alert("cancelado")
+  }
+}
