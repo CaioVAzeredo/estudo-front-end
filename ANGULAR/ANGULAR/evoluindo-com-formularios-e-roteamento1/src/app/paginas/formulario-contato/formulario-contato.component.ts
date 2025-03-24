04e2abdc061e45ca6e357832eb62005cecda201b
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-03-24 11:24:50 -0300

    aula de aungular concluida

--- ANGULAR/ANGULAR/evoluindo-com-formularios-e-roteamento1/src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ ANGULAR/ANGULAR/evoluindo-com-formularios-e-roteamento1/src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ContainerComponent } from "../../componentes/container/container.component";
-import { SeparadorComponent } from "../../componentes/separador/separador.component";
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { ContainerComponent } from '../../componentes/container/container.component';
+import { SeparadorComponent } from '../../componentes/separador/separador.component';
 
 @Component({
   selector: 'app-formulario-contato',
@@ -18,27 +19,30 @@ import { RouterLink } from '@angular/router';
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
-export class FormularioContatoComponent {
+export class FormularioContatoComponent implements OnInit{
 
   contatoForm!: FormGroup;
 
-  constructor() {
+  ngOnInit() {
+    this.inicializarFormulario();
+  }
+
+  inicializarFormulario() {
     this.contatoForm = new FormGroup({
       nome: new FormControl('', Validators.required),
       telefone: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
-      aniversario: new FormControl('', Validators.required),
-      redes: new FormControl('', Validators.required),
-      obsercacoes: new FormControl('', Validators.required),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl('')
     })
   }
 
   salvarContato() {
-    if (this.contatoForm.valid) {
-      console.log(this.contatoForm.value)
-    }
+    console.log(this.contatoForm.value);
   }
+
   cancelar() {
-    alert("cancelado")
+    console.log('Submissão cancelada');
   }
 }
--- ANGULAR/ANGULAR/evoluindo-com-formularios-e-roteamento1/src/app/paginas/lista-contatos/lista-contatos.component.ts
+++ ANGULAR/ANGULAR/evoluindo-com-formularios-e-roteamento1/src/app/paginas/lista-contatos/lista-contatos.component.ts
@@ -1,20 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
 import { CabecalhoComponent } from '../../componentes/cabecalho/cabecalho.component';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { ContatoComponent } from '../../componentes/contato/contato.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
 import { FormularioContatoComponent } from '../formulario-contato/formulario-contato.component';
-
-import agenda from '../../agenda.json';
-import { RouterLink } from '@angular/router';
+import { ContatoService } from '../../services/contato.service';
 
 interface Contato {
   id: number
   nome: string
   telefone: string
 }
+
 
 @Component({
   selector: 'app-lista-contatos',
@@ -32,11 +33,17 @@ interface Contato {
   templateUrl: './lista-contatos.component.html',
   styleUrl: './lista-contatos.component.css'
 })
-export class ListaContatosComponent {
+export class ListaContatosComponent implements OnInit{
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
-  contatos: Contato[] = agenda;
+  contatos: Contato[] = [];
 
   filtroPorTexto: string = ''
+
+  constructor(private contatoService: ContatoService) {}
+
+  ngOnInit() {
+    this.contatos = this.contatoService.obterContatos();
+  }
 
   // Remove os acentos de uma string
   private removerAcentos(texto: string): string {
